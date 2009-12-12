dfee8c5ba51b9e869d4ff72d47e7e20a3a3e2a27
Takaaki Kato <takaaki.kato@gmail.com>
2009-12-13 01:58:38 +0900

    Added a method to check if a given day is holiday or not.

--- lib/holiday_jp.rb
+++ lib/holiday_jp.rb
@@ -1,11 +1,27 @@
 require 'date'
-require 'fixtures'
 
 class HolidayJp
+  # Load holiday data
+  # TODO: require がここにあるとわかりやすい？ かえってわかりにくい？
+  require 'fixtures'
   
+  # Japanese day names from Sunday to Saturday
   DAYNAMES_JA = %w(日 月 火 水 木 金 土)
-
+  
+  # Returns all the Japanese holidays between two dates
   def self.between(start, last)
     HOLIDAYS.find_all { |hd| start <= hd[:date] && hd[:date] <= last }
   end
+end
+
+
+class Date
+  
+  # Return true if the day is a Japanese holiday
+  # Avoiding the name "jholiday?" for possible confilict with Jewish holidays
+  def japanese_holiday?
+    !HolidayJp.between(self, self).empty?
+  end
+  
+  alias_method :is_japanese_holiday?, :japanese_holiday?
 end
--- test/test_holiday_jp.rb
+++ test/test_holiday_jp.rb
@@ -8,4 +8,20 @@ class TestHolidayJp < Test::Unit::TestCase
     assert_equal holidays[1][:date], Date.new(2009, 1, 12)
     assert_equal holidays[1][:name], "成人の日"
   end
+  
+  context "#japanese_holiday?()" do
+    should "return true if the day is a Japanese holiday" do
+      assert Date.civil(2009,1,1).japanese_holiday?
+    end
+    should "return false if the day is not a Japanese holiday" do
+    assert !Date.civil(2009,1,2).japanese_holiday?
+    end
+  end
+  
+  context "#is_japanese_holiday?" do
+    should "work the same as #japanese_holiday?()" do
+      assert Date.civil(2009,1,1).is_japanese_holiday?
+      assert !Date.civil(2009,1,2).is_japanese_holiday?
+    end
+  end
 end
